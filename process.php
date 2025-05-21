cede29db3da99c2a65c47d3d8d991f1b77725804
Cj Fu <105927887@student.swin.edu.au>
2025-05-21 21:03:38 +1000

    Test12

--- process.php
+++ process.php
@@ -1,27 +1,27 @@
 <?php
 session_start();
-require_once("settings.php")
+require_once("settings.php");  
 $conn = mysqli_connect($host,$username,$password,$database);
 if (!$conn){
     die("Database connection failed: " .mysqli_connect_error());
 }
 
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        $input_username = trim($_POST['username']);
-        $input_password = trim($_POST['password']);
-        $query = "SELECT * FROM users WHERE username ='$input_username' AND password='$input_password'"
-        $result = mysqli_query($conn, $query);
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $input_username = trim($_POST['username']);
+    $input_password = trim($_POST['password']);
+    $query = "SELECT * FROM users WHERE username ='$input_username' AND password='$input_password'";  
+    $result = mysqli_query($conn, $query);
 
-        if($user = mysqli_fetch_assoc($result)){
-            $_SESSION['username'] = $user['username'];
+    if($user = mysqli_fetch_assoc($result)){
+        $_SESSION['username'] = $user['username'];
         if($user['username'] == 'placeholder'){
             header('Location:profile.php');
             exit;
-    }else{
-        $_SESSION['error'] = "Invalid username or password. Please try again."
+        }
+    } else {
+        $_SESSION['error'] = "Invalid username or password. Please try again.";  
         header('Location:login.php');
         exit;
-        }
     }
 }
 ?>
